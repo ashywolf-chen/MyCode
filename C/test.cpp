78b15cacc9c24e66169d2456462c6b1beacf8e69
ashywolf-chen <ashywolf.chen@gmail.com>
2018-04-06 00:38:35 +0800

    201804060038

---
+++ C/test.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc,char *args[])
+{
+    int i,k;
+    printf("Hello,CPP!!\n");
+    printf("argc:%d\nargv:\n",argc);
+
+    for(i=0;i<argc;i++)
+    {
+        printf("%d:%s\n",i,args[i]);
+    }
+
+    getchar();
+    return 0;
+
+}
---
+++ python/test.py
@@ -0,0 +1,23 @@
+prefix = 'Py'
+prefix = prefix + 'thon'
+print(prefix)
+print(prefix[3])
+
+print("Hello!!")
+
+username = "Yogesh"
+timestamp = "16:20"
+url = "http://petshop.com/pets/reptiles/pythons"
+
+print(username + " accessed the site " + url + " at " + timestamp + ".")
+
+given_name = "Charlotte"
+middle_names = "Hippopotamus"
+family_name = "Turner"
+
+name = given_name + " " + middle_names + " " + family_name
+
+name_length = len(name)   # todo: calculate how long this name is
+
+driving_licence_character_limit = 28
+print(name_length <= driving_licence_character_limit)
