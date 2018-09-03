b7615fb37032edb9378181fcdcd017ee8625022f
ashywolf-chen <ashywolf.chen@gmail.com>
2018-09-03 10:28:48 +0800

    20180903

---
+++ Java/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+//package com.ashywolf.sample;
+
+/**
+ * @author ashywolf
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,World!!");
+		System.out.println("Hello,Java!!");
+
+	}
+
+}
